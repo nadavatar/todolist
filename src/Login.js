e220781ca1c2892d5a25b73a53f8f75b2e60bc61
nadavatar <nadavatar64@gmail.com>
2019-08-17 14:38:48 +0300

    Routing works

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "./Components/Header";
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <p>This is where users log in</p>
+      </div>
+    );
+  }
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import Login from "./Login";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" exact component={App} />
+      <Route path="/login" exact component={Login} />
+    </div>
+  </Router>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing, document.getElementById("root"));
